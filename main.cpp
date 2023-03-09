0356044b18bbdbf9f06a2c0bcc4d6f406d3d6c0a
MaximeGeay <maxime.geay@genavir.fr>
2023-03-09 13:59:40 +0100

    Commit simple
    Modification liée à la gestion de l'icône

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icon.png"));
+    QCoreApplication::setOrganizationName("OceanCrossing");
+    QCoreApplication::setApplicationName("ConvertisseurGPS");
     MainWindow w;
     w.show();
     return a.exec();
